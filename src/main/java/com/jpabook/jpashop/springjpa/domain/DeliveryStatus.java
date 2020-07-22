3cab7c714e6407d97e30c8093b8db9d0973f152c
ChoiSuri <surichoi@hyundai-autoever.com>
2020-07-22 18:01:31 +0900

    도메인 작성

---
+++ src/main/java/com/jpabook/jpashop/springjpa/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.jpabook.jpashop.springjpa.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/com/jpabook/jpashop/springjpa/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.jpabook.jpashop.springjpa.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
